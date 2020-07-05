1a2dfd1a54cdc629683f4007f3c081fd49e49b2b
Jecé Xavier <jece@alunos.utfpr.edu.br>
2020-07-05 17:15:13 -0300

    Creating SendForgotPasswordEmailService

---
+++ backend/src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -0,0 +1,35 @@
+import { inject, injectable } from 'tsyringe';
+// import AppError from '@shared/Errors/AppError';
+import AppError from '@shared/Errors/AppError';
+import IMailProvider from '@shared/container/providers/MailProvider/models/IMailProvider';
+import IUsersRepository from '../repositories/IUsersRepository';
+import IUserTokensRepository from '../repositories/IUserTokensRepository';
+// import User from '../infra/typeorm/entities/User';
+
+interface IRequest {
+	email: string;
+}
+
+@injectable()
+export default class SendForgotPasswordEmailService {
+	constructor(
+		@inject('UsersRepository')
+		private usersRepository: IUsersRepository,
+
+		@inject('UserTokensRepository')
+		private userTokensRepository: IUserTokensRepository,
+
+		@inject('MailProvider')
+		private mailProvider: IMailProvider,
+	) {}
+
+	public async execute({ email }: IRequest): Promise<void> {
+		const userExists = await this.usersRepository.findByEmail(email);
+
+		if (!userExists) {
+			throw new AppError('User does not exist.');
+		}
+
+		await this.userTokensRepository.generate(userExists.id);
+	}
+}

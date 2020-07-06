3c4260e7b18614a0923ee7a6f251ee096efd28b3
Jecé Xavier <jece@alunos.utfpr.edu.br>
2020-07-06 17:17:47 -0300

    Updating the send mail with templates

--- backend/src/modules/users/services/SendForgotPasswordEmailService.ts
+++ backend/src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -30,9 +30,16 @@ export default class SendForgotPasswordEmailService {
 
 		const { token } = await this.userTokensRepository.generate(userExists.id);
 
-		await this.mailProvider.sendMail(
-			email,
-			`Pedido de recuperação de senha recebido ${token}`,
-		);
+		await this.mailProvider.sendMail({
+			to: {
+				name: userExists.name,
+				email: userExists.email,
+			},
+			subject: '[GoBarber] Recuperação de senha',
+			templateData: {
+				template: 'Olá, {{name}} : {{token}}',
+				variables: { name: userExists.name, token },
+			},
+		});
 	}
 }
